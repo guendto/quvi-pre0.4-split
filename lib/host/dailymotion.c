22cc9b587543fb3baa0fb077ac8b29c1f6047623
Toni Gundogdu <legatvs.git@gmail.com>
2010-02-12 11:45:29 +0200

    fix dmotion format changes.

--- lib/host/dailymotion.c
+++ lib/host/dailymotion.c
@@ -18,7 +18,7 @@
 #include "host.h"
 
 _host_constants(dailymotion,
-    "dailymotion.", "flv|spark-mini|vp6-hq|vp6-hd|vp6|h264");
+    "dailymotion.", "flv|spark-mini|h264-hq|h264-hd|h264");
 
 _host_re(re_id,     "(?i)video\\/(.*?)_");
 _host_re(re_title,  "(?i)<title>dailymotion\\s+-\\s+(.*)\\s+-");
