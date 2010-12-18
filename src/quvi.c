88925dcc90c13175d9d26045cde09be66ff76d87
Toni Gundogdu <legatvs.git@gmail.com>
2010-12-18 12:22:07 +0200

    Add publicsenat test

--- src/quvi.c
+++ src/quvi.c
@@ -480,6 +480,7 @@ static int check_values(quvi_video_t video, opts_s opts)
 }
 
 static const char *tests[] = {
+  "http://www.publicsenat.fr/vod/un-monde-de-bulles/speciale-journal-de-spirou/67141",
   "http://video.globo.com/Videos/Player/Noticias/0,,GIM1392245-7823-QUATRO+MEDICOS+SAO+PRESOS+POR+VENDER+E+USAR+PRODUTOS+FALSOS+NO+RIO,00.html",
   "http://www.cbsnews.com/video/watch/?id=7118769n",
   "http://videos.sapo.pt/hd4ZBIHG80zFviLc5YEa",
---
+++ tests/support-publicsenat.pl
@@ -0,0 +1,11 @@
+exit quviTest::run_t(
+"publicsenat",
+"--page-title",
+    "Spéciale Journal de Spirou",
+"--video-id",
+    "67141",
+"--file-length",
+    "139771016",
+"--file-suffix",
+    "flv",
+);
