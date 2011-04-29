a5e145119b139cb4d3b9f2e5c6e97c79e3f99fc6
Toni Gundogdu <legatvs.git@gmail.com>
2011-04-29 18:07:09 +0300

    Remove unused dump_lua_stack

--- lib/lua_wrap.c
+++ lib/lua_wrap.c
@@ -40,41 +40,6 @@
 #include "curl_wrap.h"
 #include "lua_wrap.h"
 
-#ifdef _0
-static void dump_lua_stack(lua_State * L)
-{
-  /* http://www.lua.org/pil/24.2.3.html */
-  int i, top;
-
-  top = lua_gettop(L);
-
-  for (i = 1; i <= top; i++)    /* repeat for each level */
-    {
-      int t = lua_type(L, i);
-      switch (t)
-        {
-        case LUA_TSTRING:          /* strings */
-          printf("`%s'", lua_tostring(L, i));
-          break;
-
-        case LUA_TBOOLEAN:         /* booleans */
-          printf(lua_toboolean(L, i) ? "true" : "false");
-          break;
-
-        case LUA_TNUMBER:          /* numbers */
-          printf("%g", lua_tonumber(L, i));
-          break;
-
-        default:                   /* other values */
-          printf("%s", lua_typename(L, t));
-          break;
-        }
-      printf("  ");               /* put a separator */
-    }
-  printf("\n");                 /* end the listing */
-}
-#endif
-
 static _quvi_media_t qv = NULL;
 
 /* "quvi" object functions for LUA scripts. */
