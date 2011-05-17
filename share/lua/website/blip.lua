dea1a0f044125b8ce78cbd2d9ae8f8a7265684e6
Toni Gundogdu <legatvs.git@gmail.com>
2011-05-17 14:53:37 +0300

    blip.lua: Fix ID parsing (related to #39)
    * Change how 'best' is determined
        - Add 'width' to algorithm
    
    * Change how 'default' format is parsed
        - Use website defined 'default' instead
    
    * Change how thumbnail URL is parsed
        - Grab URL from the config (again)
    
    * Fix fallback ID pattern
        - Replace outdated pattern

--- share/lua/website/blip.lua
+++ share/lua/website/blip.lua
@@ -24,35 +24,39 @@
 -- <https://github.com/monsieurvideo/get-flash-videos/>
 
 -- Identify the script.
-function ident (self)
+function ident(self)
     package.path = self.script_dir .. '/?.lua'
     local C      = require 'quvi/const'
     local r      = {}
     r.domain     = "blip.tv"
-    -- Usually available: source ('Source'), sd ("Blip SD"), hd ("Blip HD")
-    -- There are many other formats but those seem to be available for only
-    -- some media. 'default' to 'source'.
+    --
+    -- Typically available formats: source ('Source'), sd ("Blip SD"),
+    -- hd ("Blip HD"). There are others but these three seem to be the
+    -- most widely available ones.
+    --
+    -- 'best' is determined by parsing the available formats, picking
+    -- the one with the highest resolution. 'default' to whatever the
+    -- website defines as such.
+    --
     r.formats    = "default|best|sd|hd"
     r.categories = C.proto_http
     local U      = require 'quvi/util'
-    r.handles    = U.handles(self.page_url,
-                    {r.domain},
-                    {"/file/%d+", "/play/%w+", "/flash/%d+",
-                     "/scripts/flash/"})
+    r.handles    = U.handles(self.page_url, {r.domain},
+                    -- paths: Room for improvement.
+                    {"/file/%d+",
+                     "/play/%w+",
+                     "/flash/%d+",
+                     "/scripts/flash/",
+                     "%-%d+"})
     return r
 end
 
 -- Parse video URL.
-function parse (self)
+function parse(self)
     self.host_id  = "blip"
 
     local U = require 'quvi/util'
     self.page_url = U.unescape(self.page_url)
-
-    local page = quvi.fetch(self.page_url)
-
-    local _,_,s = page:find('rel="videothumbnail" href="(.-)"')
-    self.thumbnail_url = s or ''
 
     local _,_,id = self.page_url:find('/flash/(%d+)')
     if not id then
@@ -63,11 +67,10 @@ function parse (self)
         end
     end
 
-    if not id and page then
---        local page = quvi.fetch(self.page_url)
-        _,_,id = page:find('post_masthed_(%d+)')
+    if not id then
+        local page = quvi.fetch(self.page_url)
+        _,_,id = page:find('data%-posts%-id="(.-)"')
     end
-
     self.id = id or error('no match: video id')
 
     local config_url = 'http://blip.tv/rss/flash/' .. self.id
@@ -76,37 +79,72 @@ function parse (self)
     local _,_,s = config:find('media:title>(.-)</')
     self.title = s or error('no match: title')
 
-    local p = 'media:content url="(.-)" blip:role="(.-)" .- height="(.-)"'
-    local t = {}
-    for u,r,h in config:gfind(p) do
-        r = string.lower(r):gsub('blip ', '')
-        r = r:gsub(' %d+', '')
-        h = (h ~= nil) and tonumber(h) or 0
---        print(u,r,h)
-        t[r] = {height=h, url=u}
-    end
-
+    local _,_,s   = config:find('<blip:smallThumbnail>(.-)<')
+    self.thumbnail_url = s or ''
+ 
+    -- Choose format.
+    local found = iter_media(config)
     local r_fmt = self.requested_format
 
-    -- 'source' is our 'default'
-    local f = (r_fmt == 'default' or not t[r_fmt]) and 'source' or r_fmt
-    f = (r_fmt == 'best') and choose_best(t).role or f
-    self.url =  {t[f].url}
+    local url = (r_fmt == 'default' or not found[r_fmt])
+                    and choose_default(found)
+                    or  found[r_fmt].url
+
+    url = (r_fmt == 'best')
+            and choose_best(found).url
+            or  url
+
+    self.url = {url or error('no match: media url')}
 
     return self
 end
+ 
+function iter_media(config)
+    local p = '<media:content url="(.-)"'
+           .. '.-blip:role="(.-)"'
+           .. '.-height="(.-)"'
+           .. '.-isDefault="(.-)"'
+           .. '.-width="(.-)"'
+    local t = {}
+    for u,r,h,d,w in config:gfind(p) do
+        r = string.lower(r):gsub('blip%s+','')
+        r = r:gsub('%s+%d+','')
+        r = r:gsub(' ','_')
+--        print(string.format('(%s)',r))
+        w = (w ~= nil) and tonumber(w) or 0
+        h = (h ~= nil) and tonumber(h) or 0
+--        print('iter_media:', u, r, w, h, d)
+        t[r] = {width=w, height=h, url=u, default=d}
+    end
+    return t
+end
 
 function choose_best(t)
-    local best = {role=nil, height=0, url=nil}
+    local best = {role=nil, width=0, height=0, url=nil}
     for k,v in pairs(t) do
-        if best.height <= v.height then
-            best.height = v.height
-            best.url    = v.url
-            best.role   = k
+        if v.height > best.height then
+            if v.width >  best.width then
+                best.width  = v.width
+                best.height = v.height
+                best.url    = v.url
+                best.role   = k
+            end
         end
     end
---    print(best.role, best.url, best.height)
+--    print('choose_best:', best.url, best.role, best.width, best.height)
     return best
+end
+ 
+function choose_default(t)
+    local url
+    for k,v in pairs(t) do
+        if v.default == 'true' then
+            url = v.url
+            break
+        end
+    end
+--    print('choose_default:', url)
+    return url
 end
 
 -- vim: set ts=4 sw=4 tw=72 expandtab:
