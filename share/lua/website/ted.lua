61500adaa42fd334241bf35757b5fa29114987fc
Toni Gundogdu <legatvs.git@gmail.com>
2011-05-30 14:05:55 +0300

    ted.lua: Rewrite for quvi_query_formats

--- share/lua/website/ted.lua
+++ share/lua/website/ted.lua
@@ -1,5 +1,6 @@
 
 -- quvi
+-- Copyright (C) 2011  Toni Gundogdu <legatvs@gmail.com>
 -- Copyright (C) 2011  Bastien Nocera <hadess@hadess.net>
 --
 -- This file is part of quvi <http://quvi.sourceforge.net/>.
@@ -20,6 +21,8 @@
 -- 02110-1301  USA
 --
 
+local Ted = {} -- Utility functions unique to this script
+
 -- Identify the script.
 function ident(self)
     package.path = self.script_dir .. '/?.lua'
@@ -33,39 +36,95 @@ function ident(self)
     return r
 end
 
+-- Query available formats.
+function query_formats(self)
+    local page    = quvi.fetch(self.page_url)
+    local formats = Ted.iter_formats(page)
+
+    local t = {}
+    for _,v in pairs(formats) do
+        table.insert(t, Ted.to_s(v))
+    end
+
+    table.sort(t)
+    self.formats = table.concat(t, "|")
+
+    return self
+end
+
 -- Parse video URL.
 function parse(self)
     self.host_id  = "ted"
 
     local page = quvi.fetch(self.page_url)
 
+    local _,_,s = page:find('ti:"(%d+)"')
+    self.id = s or error("no match: media id")
+
     local _,_,s = page:find('<span id="altHeadline">(.-)</span>')
     local U      = require 'quvi/util'
-    self.title  = U.unescape(s) or error("no match: video title")
+    self.title  = U.unescape(s) or error("no match: media title")
 
     local _,_,s = page:find('&amp;su=(.-)&amp;')
     self.thumbnail_url = s or ""
 
-    local _,_,s = page:find('<h3>Video</h3>.-dl class="downloads"(.-)</dl>')
-    local metadata = s or error("no match: metadata")
-
-    local _,_,s =
-        metadata:find('<a href="([^ .]-)">Download video to desktop')
-
-    local path  = s or error("no match: standard src") -- 'default'
-
-    if self.requested_format == 'best' then
-        local _,_,s =
-            metadata:find('<a href="([^ .]-)">Watch high%-res video')
-        path = s or path -- fallback to 'default'
-    end
-
-    self.url = {"http://www.ted.com" .. path}
-
-    local _,_,s = path:find("talk/(.+)$")
-    self.id = s or error("no match: video id")
+    local formats = Ted.iter_formats(page)
+    self.url      = {U.choose_format(self, formats,
+                                     Ted.choose_best,
+                                     Ted.choose_default,
+                                     Ted.to_s).url
+                        or error("no match: media url")}
 
     return self
+end
+
+--
+-- Utility functions
+--
+
+function Ted.iter_formats(page)
+    --
+    -- Assume first mp4 to be the 'sd' and the following (if any) the
+    -- 'hd'. Some media have also audio tracks (e.g. 'mp3').
+    --
+    local have_sd = false
+    local t = {}
+    for src,c in page:gfind('href="(.-)">.-%((%w+)%)') do
+        if src:find('/download/') then
+            local u,q = 'http://www.ted.com' .. src
+            c         = string.lower(c)
+            if c == 'mp4' then
+                q = have_sd and 'sd' or 'hd'
+                have_sd = true
+            end
+--            print(u,c,q)
+            table.insert(t, {url=u, container=c, quality=q})
+        end
+    end
+    return t
+end
+
+function Ted.choose_best(formats) -- Last is the 'best'
+    local r
+    for _,v in pairs(formats) do r = v end
+    return r
+end
+
+function Ted.choose_default(formats) -- First 'mp4' is the 'default'
+    local r -- Return this if mp4 is not found for some reason
+    for _,v in pairs(formats) do
+        if v.container == 'mp4' then
+            return v
+        end
+        r = v
+    end
+    return r
+end
+
+function Ted.to_s(t)
+    return (t.quality)
+            and string.format("%s_%s", t.container, t.quality)
+            or  string.format("%s", t.container)
 end
 
 -- vim: set ts=4 sw=4 tw=72 expandtab:
