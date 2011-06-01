91b1c678d82bda25f209ebf7c6b23e00bbf75786
Toni Gundogdu <legatvs.git@gmail.com>
2011-06-01 10:06:48 +0300

    llst.h: Add nd comments

--- src/libquvi/quvi/llst.h
+++ src/libquvi/quvi/llst.h
@@ -20,16 +20,12 @@
 #ifndef quvi_llst_h
 #define quvi_llst_h
 
-/** @defgroup libquvi_llst Linked list interface
- *
- * The linked list interface.
- *
- * @since 0.2.16
- * @{
- */
+/* Title: llst.h */
 
-/** @brief Linked list node handle
- * @since 0.2.16
+/*
+ * Typedef: Handles
+ *
+ * quvi_llst_node_t Node handle
  */
 typedef void *quvi_llst_node_t;
 
@@ -37,71 +33,15 @@ typedef void *quvi_llst_node_t;
 extern "C" {
 #endif /* __cplusplus */
 
-  /** @brief Append new node to a linked list
-   *
-   * @param llst Handle to a linked list node (if NULL, new linked list is created)
-   * @param data Data to be appended to the list
-   *
-   * Example:
-   * @code
-   * quvi_llst_node_t l = NULL;
-   * quvi_llst_append(l, strdup("foo"));
-   * quvi_llst_append(l, strdup("bar"));
-   * @endcode
-   *
-   * @return Non-zero if an error occurred
-   *
-   * @since 0.2.16
-   *
-   * @warning Make sure that the data is allocated dynamically
-   *
-   * @sa quvi_llst_free
-   */
-  QUVIcode quvi_llst_append(quvi_llst_node_t *llst, void *data);
-
-  /** @brief Return the size (number of nodes) in linked list
-   *
-   * @param llst Handle to a linked list node
-   *
-   * @return Number of nodes
-   *
-   * @since 0.2.16
-   */
-  size_t quvi_llst_size(quvi_llst_node_t llst);
-
-  /** @brief Return next linked node
-   *
-   * @param llst Handle to a linked list node
-   *
-   * @return Handle to the next linked list (or NULL)
-   *
-   * @since 0.2.16
-   */
-  quvi_llst_node_t quvi_llst_next(quvi_llst_node_t llst);
-
-  /** @brief Return data of the linked list node
-   *
-   * @param node Handle to a linked list node
-   *
-   * @return Pointer to the node data
-   *
-   * @since 0.2.16
-   */
-  void *quvi_llst_data(quvi_llst_node_t node);
-
-  /** @brief Release memory allocated by the linked list
-   *
-   * @param llst Handle to a linked list node
-   *
-   * @since 0.2.16
-   */
-  void quvi_llst_free(quvi_llst_node_t *llst);
+  QUVIcode         quvi_llst_append(quvi_llst_node_t *node, void *data);
+  size_t           quvi_llst_size(quvi_llst_node_t node);
+  quvi_llst_node_t quvi_llst_next(quvi_llst_node_t node);
+  void            *quvi_llst_data(quvi_llst_node_t node);
+  void             quvi_llst_free(quvi_llst_node_t *node);
 
 #ifdef __cplusplus
 }
 #endif /* __cplusplus */
-
-/** @} */ /* End of libquvi_llst group. */
 
 #endif /* quvi_llst_h */
 
